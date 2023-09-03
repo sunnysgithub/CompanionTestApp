1d4b47a790b3baf0105c92a24285c5593b4693d9
Sunny Singh <sunnysingh@posteo.de>
2023-09-03 15:04:13 +0200

    Initial Commit

---
+++ CompanionTestApp Watch App/CompanionTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionTestAppApp.swift
+//  CompanionTestApp Watch App
+//
+//  Created by Sunny Singh on 03.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CompanionTestApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompanionTestApp/CompanionTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionTestAppApp.swift
+//  CompanionTestApp
+//
+//  Created by Sunny Singh on 03.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CompanionTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
